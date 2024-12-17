b349e012a51e8f1f4cd552a69bc3d39f061f950c
kyand38 <kyand2024@gmail.com>
2024-12-17 11:53:00 -0600

    Added a paragraph to the porfolio page and an @media query for the name text in the header on same page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Navigation from './Navigation'
 
 
+//This function returns the header component of the application
 function Header() {
     const name = 'Kyle Anderson';
     return (
         <header className="border-bottom rounded border-info bg-dark text-info py-4">
             <div className="container d-flex justify-content-between align-items-center">
-                <h1 className="m-0 shadow-sm">{name}</h1>
+                <h1 className="m-0 shadow-sm kyleStyle">{name}</h1>
                 <Navigation />
             </div>
         </header>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -61,7 +61,19 @@ const Portfolio = () => {
 
   return (
     <div className="container">
-      <h2 className="text-center my-5">My Projects</h2>
+      <h2 className="text-center my-5 text-info">My Projects</h2>
+      <div className="container border border-info rounded p-3">
+      <text className="text-info text-center my-5">
+          Thank you for taking the time to check out my work.
+         Some of these projects have a link to a video walk through of a terminal/node-based application.
+         Some of my projects are being hosted either by Netlify or Render. 
+         Currently this website is my most complete as while I was flying through class I was focused on getting the minimum project requirements done before moving on to future development goals.
+         That being said I am always looking for ways to improve my projects and my skills. 
+         Because these applications are being hosted on free hosting sites they may shut down after a period of inactivity.
+         I'm trying to stay on top of that but if you find a project that is not working please let me know.
+         I hope you enjoy my work and I look forward to working with you in the future.
+     </text>
+     </div>
       <div className="d-flex flex-wrap justify-content-center">
         {projects.map((project, index) => (
           <Card
